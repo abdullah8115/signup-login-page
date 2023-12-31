c5a15004853b7aadca30e06b3e6aa6f49479fec6
Abdullah Mairaj <141283430+abdullah8115@users.noreply.github.com>
2023-12-31 14:26:18 +0500

    Class 31-12-23

--- index.js
+++ index.js
@@ -7,21 +7,101 @@ function openTab(tabName) {
     document.getElementById(tabName).style.display = "block";
 }
 
+
+
+function signupUser() {
+    var newUsername = document.getElementById("newUsername").value;
+    var newPassword = document.getElementById("newPassword").value;
+    var age = document.getElementById("age").value;
+    var birthDate = document.getElementById("birthDate").value;
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var email = document.getElementById("email").value;
+
+    console.log("Username: " + newUsername);
+    console.log("Password: " + newPassword);
+    console.log("Age: " + age);
+    console.log("Birth Date: " + birthDate);
+    console.log("First Name: " + firstName);
+    console.log("Last Name: " + lastName);
+    console.log("Email: " + email);
+
+    let Userdata = {
+        Username: newUsername,
+        Password: newPassword,
+        Age: parseInt(age),
+        BirthDate: birthDate,
+        FirstName: firstName,
+        LastName: lastName,
+        Email: email
+    }
+
+    console.log(Userdata)
+
+    var AllUsers = localStorage.getItem("allusers");
+
+    if (AllUsers === null) {
+
+        AllUsers = [];
+    } else {
+
+        AllUsers = JSON.parse(AllUsers);
+    }
+
+    AllUsers.push(Userdata);
+
+    localStorage.setItem("allusers", JSON.stringify(AllUsers));
+
+    console.log(AllUsers);
+
+    alert("Your account has been created!");
+
+    // return false; 
+}
+
+var NewUsers = []
+
 function loginUser() {
     var username = document.getElementById("loginUsername").value;
     var password = document.getElementById("loginPassword").value;
 
     console.log("Login: " + username);
-    return false;
+    console.log("Password: " + password);
+
+    var allusers = localStorage.getItem("allusers");
+
+    if (allusers === null) {
+        alert("Please Create an Account!");
+        return;
+    }
+
+    allusers = JSON.parse(allusers);
+
+    var foundUser = null;
+
+    for (var i = 0; i < allusers.length; i++) {
+        if (allusers[i].Username === username) {
+            foundUser = allusers[i];
+            alert("USER FOUND");
+            break;
+        }
+    }
+
+    if (foundUser) {
+
+        // User found, check password
+
+        if (foundUser.Password === password) {
+            alert("Your account is logged in now!");
+        } else {
+            alert("Incorrect password. Please try again.");
+        }
+    } else {
+        alert("Username not found. Please create a new account!");
+    }
 }
 
-function signupUser() {
-    var newUsername = document.getElementById("newUsername").value;
-    var newPassword = document.getElementById("newPassword").value;
 
-    console.log("Signup: " + newUsername);
-    return false; 
-}
 
 openTab("login");
 
@@ -32,11 +112,3 @@ function openSignupPopup() {
 function closeSignupPopup() {
     document.getElementById("signupPopup").style.display = "none";
 }
-
-function signupUser() {
-
-    closeSignupPopup();
-    alert("Your account has been created!");
-    return false;
-}
-
